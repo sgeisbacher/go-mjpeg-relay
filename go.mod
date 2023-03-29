19eea79bbec758fb7fff9eb18d0612a61e6f4139
Stefan Geisbacher <stefan.geisbacher@gmail.com>
2023-03-29 20:57:10 +0200

    added rendering text to no-signal-stream

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/sgeisbacher/go-mjpeg-relay
 
 go 1.19
 
-require github.com/mattn/go-mjpeg v0.0.3
+require (
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/mattn/go-mjpeg v0.0.3
+	golang.org/x/image v0.6.0
+)
--- textSource.go
+++ textSource.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"bytes"
-	"image"
+	"fmt"
 	"image/color"
-	"image/draw"
 	"image/jpeg"
+	"log"
+	"time"
+
+	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
 var (
+	white  = color.RGBA{255, 255, 255, 255}
 	blue   = color.RGBA{0, 0, 255, 255}
 	red    = color.RGBA{255, 0, 0, 255}
 	green  = color.RGBA{0, 255, 0, 255}
@@ -26,11 +32,42 @@ func (source *TextBroadcasterSource) Init() error {
 func (source *TextBroadcasterSource) ReadFrame() ([]byte, error) {
 	w := 800
 	h := 600
-	color := blue
-	im := image.NewRGBA(image.Rect(0, 0, w, h))
-	draw.Draw(im, im.Bounds(), &image.Uniform{color}, image.ZP, draw.Src)
+	// color := blue
+	// img := image.NewRGBA(image.Rect(0, 0, w, h))
+	// draw.Draw(img, img.Bounds(), &image.Uniform{color}, image.ZP, draw.Src)
+	font, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// animate dots to show activity
+	txt := source.text
+	dots := time.Now().Second() % 4
+	for i := 0; i < dots; i++ {
+		txt = fmt.Sprintf("%s.", txt)
+	}
+	for i := 0; i < 3-dots+1; i++ {
+		txt = fmt.Sprintf("%s ", txt)
+	}
+
+	// start drawing
+	face := truetype.NewFace(font, &truetype.Options{Size: 48})
+
+	dc := gg.NewContext(w, h)
+
+	// draw blue background
+	dc.SetColor(blue)
+	dc.Clear()
+
+	// draw text
+	dc.SetFontFace(face)
+	dc.SetColor(white)
+	dc.DrawStringAnchored(txt, 412, 412, 0.5, 0.5)
+
+	img := dc.Image()
+
 	var buff bytes.Buffer
-	jpeg.Encode(&buff, im, nil)
+	jpeg.Encode(&buff, img, nil)
 	return buff.Bytes(), nil
 }
 
